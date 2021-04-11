a7627d1c3a68c7237ed179487347ba8b1fc7fd79
upasana-dev <81856416+upasana-dev@users.noreply.github.com>
2021-04-11 09:27:12 +0800

    Improved pre-populated data
    Updated data populated at startup to include more realistic data

--- aequilibrium/src/main/resources/data.sql
+++ aequilibrium/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (1, 8, 5, 6, 3, 2, 1, 9, 4, 'A', 'Optimus Prime',20);
-INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (2, 8, 5, 6, 3, 2, 1, 9, 7, 'A', 'Bumblebee',20);
-INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (3, 8, 5, 6, 3, 2, 1, 9, 8, 'A', 'Storm Trooper',20);
+INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (1, 8, 5, 6, 3, 1, 1, 9, 4, 'A', 'Optimus Prime',20);
+INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (2, 7, 5, 5, 6, 9, 8, 7, 10, 'A', 'Bumblebee',30);
+INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (3, 6, 4, 8, 6, 7, 7, 8, 7, 'A', 'Ironhide',32);
 
-INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (4, 8, 5, 6, 3, 2, 1, 9, 6, 'D', 'Megatron',20);
-INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (5, 8, 5, 6, 3, 2, 1, 9, 2, 'D', 'Megatron Jr',20);
-INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (6, 8, 5, 6, 3, 2, 1, 9, 10, 'D', 'Megan Thee Stallion',20);
+INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (4, 9, 3, 8, 6, 6, 7, 5, 7, 'D', 'Starscream',31);
+INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (5, 8, 5, 6, 3, 2, 1, 9, 2, 'D', 'Predaking',31);
+INSERT INTO transformer (id, strength, intelligence, speed, endurance, rank, courage, firepower, skill, transformer_type, name, overall_rating) VALUES (6, 6, 8, 8, 5, 4, 9, 7, 9, 'D', 'Megatron',34);
